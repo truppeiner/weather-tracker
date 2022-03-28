2347d815f431640a7f971e17aac6cbd049e652b9
Thomas Ruppeiner <truppeiner@gmail.com>
2022-03-28 13:44:27 -0500

    added buttons and first fetch

--- assets/jss/java.js
+++ assets/jss/java.js
@@ -11,4 +11,19 @@
 //store lat and lon
 //grab by ID and pass template literal 
 //example of template literal 
-`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}`
+// `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}`
+
+var cityname = 'austin';
+var openWeatherCityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=f969ae2c4737df8d7f3f538180cd201c`;
+
+fetch(openWeatherCityUrl).then(function(response){
+    //if request successful 
+    if(response.ok){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    } else {
+        console.log('error: '+ response.statusText);
+    }
+})
+
